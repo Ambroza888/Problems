34f7def4581070b09f36c393bcf7d4a964bbbc28
Veso <Ambroza8888@gmail.com>
2020-04-09 13:12:40 -0700

    hello

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            consolewriteline.say();
         }
     }
 }
---
+++ qwe.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Problems
+{
+  public class consolewriteline
+  {
+    public static void say()
+    {
+      System.Console.WriteLine("hello from the other side");
+    }
+  }
+}
