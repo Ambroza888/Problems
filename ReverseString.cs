9227ea3333e7de07f158ec11d3542157b362475f
Veso <Ambroza8888@gmail.com>
2020-04-15 13:40:18 -0700

    reversestring

---
+++ ReverseString.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems
+{
+  class ReverseString
+  {
+    public static void option1(string str)
+    {
+      //[hello] [hellh] [oellh]
+      char[] charstr = str.ToCharArray();
+      int l = str.Length/2;
+      for(int i = 0; i < l ; i++)
+      {
+
+      }
+    }
+  }
+}
